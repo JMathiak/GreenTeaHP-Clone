beda0d23c4580a24e75aa0a1a240ba4500e80c53
Josh Mathiak <jmathiak4@gmail.com>
2023-02-07 12:09:40 -0600

    added other dropdowns to the nav bar

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,11 +1,27 @@
 function addHover() {
-  let dropdown = document.querySelector("#sem-dropdown");
+  let semDropdown = document.querySelector("#sem-dropdown");
   let semNavBar = document.querySelector("#sem");
+  let mlDropdown = document.querySelector("#ml-dropdown");
+  let mlNavBar = document.querySelector("#ml");
+  let sogtDropdown = document.querySelector("#sogt-dropdown");
+  let sogtNavBar = document.querySelector("#sogt");
   semNavBar.addEventListener("mouseover", function (e) {
-    dropdown.classList.remove("hidden");
+    semDropdown.classList.remove("hidden");
   });
   semNavBar.addEventListener("mouseout", function (e) {
-    dropdown.classList.add("hidden");
+    semDropdown.classList.add("hidden");
+  });
+  mlNavBar.addEventListener("mouseover", function (e) {
+    mlDropdown.classList.remove("hidden");
+  });
+  mlNavBar.addEventListener("mouseout", function (e) {
+    mlDropdown.classList.add("hidden");
+  });
+  sogtNavBar.addEventListener("mouseover", function (e) {
+    sogtDropdown.classList.remove("hidden");
+  });
+  sogtNavBar.addEventListener("mouseout", function (e) {
+    sogtDropdown.classList.add("hidden");
   });
 }
 
